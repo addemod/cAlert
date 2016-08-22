69635397d6daa504c8ffabe337942a469fe94204
Adam <a.clettborn@gmail.com>
2016-08-22 12:46:29 +0200

    Fixed the queue bug
    There was a bug where you removed the alert by clicking it and it made the queue system fail.
    It has now been fixed and manual removing is working!

--- js/cAlert.js
+++ js/cAlert.js
@@ -72,53 +72,78 @@ var cAlert = function(body, type, icon = "bubble2", time = 2) {
 		{
 			// Launch alert on the alert on index 0
 			alertQueue[0].alert();
-			// When the alert is removed
-			alertQueue[0].onremove = function() { 
-				// Remove the alert on index 0
-				alertQueue.splice(0, 1);
-				// If we have another alert in queue (on index 0 after we removed the first one)
-				if(alertQueue[0])
-				{
-					// Queue that alert
-					alertQueue[0].queue();
-				}
-			};
 		}
 		
 	};
 
 	this.dismiss = function() {
-		var _this = this;
+		// Call the ondismiss function
 		if(this.ondismiss() === false)
 		{
 			return;
 		}
-
+	
+		// Make the alert go up and then we remove it
+		// We also call the ondismissed function
 		this.elem.stop().animate({top: 0 - this.elem.height()}, 500, function() { _this.ondismissed(); _this.remove(); });
 	}
 
 	this.remove = function() {
+		// Check if the alert is in the queue
+		if(this.isInQueue())
+		{
+			// If the alert is in the queue we remove it from the queue
+			// when the alert is removed.
+			var index = getIndexAlertQueue(this.id);
+			alertQueue.splice(index, 1);
+			// Check if there's another one in the queue
+			if(alertQueue[0])
+			{
+				// Queue that alert
+				alertQueue[0].queue();
+			}
+		}
+		
+		// Call the onremove function
 		if(this.onremove() === false)
 		{
 			return;
 		}
 		
+		// Remove the element from the body
 		this.elem.remove();
 	}
 
 	this.click = function() {
+		// Call the onclick function
 		if(this.onclick() === false)
 		{
 			return;
 		}
 		
+		// If the alert has a timer until it is being removed
 		if(this.hideTimeout)
 		{
+			// Remove the timeout when we click the alert
 			clearTimeout(this.hideTimeout);
 		}
-
+		
+		// Call the remove function
 		this.remove();
 	}
+	
+	this.isInQueue = function() {
+		// Get the index of the alert in the alertQueue list (if it exists, otherwise it returns -1)
+		var index = getIndexAlertQueue(this.id);
+		// If index is greater than -1
+		if(index > -1)
+		{
+			// The alert is in the queue list, index would've been -1 if it wasn't in the list
+			return true;
+		}
+		// The index was not greater than -1, return false
+		return false;
+	};
 
 	// When the alert() function is called
 	this.onalert = function() {};
@@ -142,17 +167,19 @@ var cAlert = function(body, type, icon = "bubble2", time = 2) {
 	this.onremove = function() {};
 }
 
-function getAlertById(id)
+function getIndexAlertQueue(id)
 {
-	for(anAlert of alerts)
+	var index = 0;
+	for(anAlert of alertQueue)
 	{
 		if(anAlert.id === id)
 		{
-			return anAlert;
+			return index;
 		}
+		index++;
 	}
 	
-	return undefined;
+	return -1;
 }
 
 function generateUUID() {
